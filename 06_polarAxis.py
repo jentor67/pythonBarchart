89c34848d69489f28612e076cb5b9f906edcbc9f
John Major <john.jentor@gmail.com>
2024-02-22 23:14:34 -0600

    correction

--- 06_polarAxis.py
+++ 06_polarAxis.py
@@ -10,6 +10,7 @@ def main():
     loudnessOfNote=[0,1,1,8,6,7,5,5,7,2,2,3,3,1,2,3,9,9,3,3] 
     lengthOfNote=[2,1,3,2,2,1,1,2,2,1,1,1,3,3,2,1,1,1,1,2]
 
+    
     # Compute pie slices
     N = 20
     theta = [(i/36)*2*np.pi for i in timeOfNote]
