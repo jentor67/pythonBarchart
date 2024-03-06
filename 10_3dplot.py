5f6f1dfdd97daa664d559d06286f5d456873519a
John Major <john.jentor@gmail.com>
2024-03-05 22:17:05 -0600

    3d barplot

---
+++ 10_3dplot.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+# import library
+import numpy as np
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from icecream import ic
+
+
+def main():
+
+    fig = plt.figure(figsize=(10,6))
+    ax = fig.add_subplot(111, projection='3d')
+
+    # create number 0 - 5 step 1
+    start = .02
+    end = 1
+    step = .02
+    x = np.arange(start, end + step, step)
+  
+    # List of Battery ratings
+    equations = ['x', 'x^2', 'x^3', 'x^4', 'x^5']
+ 
+    Y = np.empty((0,5))
+
+    for i in x:
+        line =[]
+        line.append(i)
+        line.append(i**2)
+        line.append(i**3)
+        line.append(i**4)
+        line.append(i**5)
+        Y = np.vstack((Y,np.array(line))) 
+
+
+    # Represent battery rating in numeric codes
+    yticks = [0, 1, 2, 3, 4]
+    
+    # Use different color for each of the battery ratings
+    colors = ['r', 'g', 'b', 'y', 'm']
+    
+    i=0
+    for c, k in zip(colors, yticks):
+        cs = [c] * len(x)
+        ax.bar(x, Y[:,i], width = .8*step, zs=k, zdir='y', color=cs, \
+                alpha=0.7)
+        i += 1
+
+    ax.set_yticks(yticks)
+    ax.set_yticklabels(equations)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Equations')
+    ax.set_zlabel('Y value')
+
+
+    # Show plot
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    main()
+
